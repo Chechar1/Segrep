82abd60cf7976223f6fe8cc6290ef899b58f0e9b
Chechar1 <c.clemente1@hotmail.com>
2020-07-15 21:10:12 -0500

    Se actualizo la tabla de registros para las migraciones

--- database/migrations/2020_06_24_175708_create_registros_table.php
+++ database/migrations/2020_06_24_175708_create_registros_table.php
@@ -15,6 +15,11 @@ class CreateRegistrosTable extends Migration
     {
         Schema::create('registros', function (Blueprint $table) {
             $table->integer('category_id')->foreign('user_id')->references('id')->on('users');
+            $table->string('cpu');
+            $table->string('memtotal');
+            $table->string('memus');
+            $table->string('memlib');
+            $table->string('disco');
             $table->timestamps();
         });
     }
