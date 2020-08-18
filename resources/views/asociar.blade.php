fc16fdb00e264dbb651d41e3f967bc91f053a19e
Chechar1 <c.clemente1@hotmail.com>
2020-08-18 18:32:41 -0500

    Ajuste

--- resources/views/asociar.blade.php
+++ resources/views/asociar.blade.php
@@ -8,27 +8,21 @@
                 @csrf
                     <div class='form-group'>
                         <label for="name">{{ 'Administrador' }}</label>
-                        <input class='form-control @error('name') is-invalid @enderror"' type="select" name="name" id="name">
+                        <input class='form-control @error('name') is-invalid @enderror"' type="number" name="user_id" id="user_id">
                     @error('name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
-                    @foreach ($datos as $dato)
-                        <option value="{{ $dato->id }}"> {{ $dato->id }}</option>
-                    @endforeach
                     </div>
                     <div class='form-group'>
                         <label for="name">{{ 'Servidor' }}</label>
-                        <input class='form-control @error('server') is-invalid @enderror"' type="select" name="server" id="server">
+                        <input class='form-control @error('server') is-invalid @enderror"' type="number" name="server_id" id="server_id">
                     @error('name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                     @enderror
-                    @foreach ($datoservers as $datoserver)
-                        <option value="{{ $datoserver->id }}"> {{ $datoserver->id }}</option>
-                    @endforeach
                     </div>
                     <button type="submit" class="btn btn-primary">Registrar</button>
                 </form>
