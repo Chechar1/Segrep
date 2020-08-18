e605d63a115f6725db195624e5cc578a2ef1f8d1
Chechar1 <c.clemente1@hotmail.com>
2020-08-18 18:54:31 -0500

    Ajuste servidor

--- resources/views/server.blade.php
+++ resources/views/server.blade.php
@@ -17,7 +17,7 @@
                     </div>
                     <div class='form-group'>
                         <label for="name">{{ 'Ip' }}</label>
-                        <input class='form-control @error('ip') is-invalid @enderror"' type="text" name="ip" id="ip" placeholder="Ingrese la ip">
+                        <input class='form-control @error('name') is-invalid @enderror"' type="text" name="ip" id="ip" placeholder="Ingrese la ip">
                     @error('name')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
