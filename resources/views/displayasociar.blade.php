1ffc2ae7c3e0302d42514fb3330b0fa0b28381de
Chechar1 <c.clemente1@hotmail.com>
2020-08-18 20:30:46 -0500

    Id

--- resources/views/displayasociar.blade.php
+++ resources/views/displayasociar.blade.php
@@ -16,7 +16,7 @@
 
                     <td class="v-align-middle">
 
-                        <form action="{{ route('eliminarasociar', $server->id) }}" method="POST" class="form-horizontal" role="form">
+                        <form action="{{ route('eliminarasociar', $server->user_id) }}" method="POST" class="form-horizontal" role="form">
 
                             <input type="hidden" name="_method" value="PUT">
                             <input type="hidden" name="_token" value="{{ csrf_token() }}">
